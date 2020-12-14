7f7325c9969a632bda930624ca55437373c04e1b
LoicLabaisse <labaisse.loic@gmail.com>
2020-12-14 09:55:04 +0100

    labaise_loic_quest010

--- front/simpson_odyssey/src/App.js
+++ front/simpson_odyssey/src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SignUp from "./components/SignUp";
 
 function App() {
-  return <div className='App'>Home</div>;
+  return (
+    <div className='App'>
+      Home
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
---
+++ front/simpson_odyssey/src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>test@test.com</h1>
+      <input type='email' name='email' />
+    </div>
+  );
+};
+
+export default SignUp;
