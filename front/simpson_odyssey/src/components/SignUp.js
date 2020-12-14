321208fa55d346f961ead14401cb18e3a7e559c7
LoicLabaisse <labaisse.loic@gmail.com>
2020-12-14 12:21:54 +0100

    labaisse_loic_quest020

--- front/simpson_odyssey/src/components/SignUp.js
+++ front/simpson_odyssey/src/components/SignUp.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: "",
+    lastname: "",
+  });
+
+  const updateEmailField = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div>
-      <h1>test@test.com</h1>
-      <input type='email' name='email' />
+      <h1>{JSON.stringify(form, 1, 1)}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='email' name='email' onChange={updateEmailField} />
+        <input type='password' name='password' onChange={updateEmailField} />
+        <input type='name' name='name' onChange={updateEmailField} />
+        <input type='lastname' name='lastname' onChange={updateEmailField} />
+        <input type='submit' value='Soumettre' />
+      </form>
     </div>
   );
 };
