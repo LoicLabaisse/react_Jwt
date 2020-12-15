c567bda8ccac2bb9dc8d058d87636b3940ca2d26
LoicLabaisse <labaisse.loic@gmail.com>
2020-12-15 20:36:35 +0100

    labaisse_loic_quest040

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -1,8 +1,12 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "5Gl2k32cmnwx@",
-  database: "jwt_quest",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 module.exports = connection;
